630470353da166abe115f21a7ee7296ecc503204
Jsheriff20 <38663371+Jsheriff20@users.noreply.github.com>
2019-09-13 20:32:39 +0100

    Initial Commit

---
+++ coursework/nav_bar.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+    <nav class= "navbar navbar-expand-md navbar-dark navbar-default">
+
+        <div class="container">
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav">
+
+                 <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div class="collapse navbar-collapse">
+
+                <div class="navbar-nav">
+
+                    <a class="nav-item nav-link" href="../../index.php">Home</a>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 1 
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 2 
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 3 
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 4 
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 5 
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 6 
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 7 
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 8 
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 9 
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 10 
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 11
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+
+
+                    <div class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbar_dropdown_menu_link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Week 12
+                        </a>
+
+
+                        <div class="dropdown-menu" aria-labelledby="navbar_dropdown_menu_link">
+                            
+                            <a class="dropdown-item" href=".php"></a>
+                            <a class="dropdown-item" href=".php">Commentary</a>
+                       </div>
+                    </div>
+                </div>                        
+            </div>
+        </div>
+    </nav>        
+</html>
